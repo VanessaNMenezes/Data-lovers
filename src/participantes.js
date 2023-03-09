d592c74121e2a07de11e5639215d92cc66ac6beb
vanessa <vanessa_menezes_18@hotmail.com>
2023-03-09 19:19:51 -0300

    finaliza testes

--- src/participantes.js
+++ src/participantes.js
@@ -1,7 +1,7 @@
 import {filterGender,filterData,filterAge,ascendingOrder,descendingOrder} from './data.js';
 import data from './data/athletes/athletes.js';
 
-const tableCreated = document.querySelector('#tableFilter');
+const tableCreated = document.getElementById('tableFilter');
 const genderSelector = document.querySelector('#selectGender');
 const ageSelector = document.querySelector('#selectAge');
 const orderSelector = document.querySelector('#selectOrder'); 
@@ -13,13 +13,12 @@ const arrayAthletes = data.athletes;
 endButton.onclick = () => {
 
   if (genderSelector.value === "" || ageSelector.value === "" || orderSelector.value === "") {
-  selectAllOptions.innerHTML = "⚠️ Por favor, selecione todas as opções antes de clicar em OK"
+    selectAllOptions.innerHTML = "⚠️ Por favor, selecione todas as opções antes de clicar em OK";
   }
-  else  if (genderSelector.value === "" || ageSelector.value !== "" || orderSelector.value !== "") {
+  else if (genderSelector.value === "" || ageSelector.value !== "" || orderSelector.value !== "") {
     selectAllOptions.innerHTML = "⚠️ Por favor, selecione todas as opções antes de clicar em OK";
  
   } else {
-
     const gender = filterGender(arrayAthletes, "gender", genderSelector.value);
     const filterOne = filterAge(gender, ageSelector.value);
     const filtered = filterData(filterOne, "gender", genderSelector.value);
@@ -29,42 +28,41 @@ endButton.onclick = () => {
       sortedAthletes = ascendingOrder(filtered);
     } else {
       sortedAthletes = descendingOrder(filtered);
-      }  
+    }  
 
     tableCreated.innerHTML =  `
-      <table>
-        <thead>
+    <table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Gênero</th>
+          <th>Altura</th>
+          <th>Peso</th>
+          <th>Esporte</th>
+          <th>País</th>
+          <th>Sigla</th>
+          <th>Idade</th>
+          <th>Evento</th>
+          <th>Medalha</th>
+        </tr>
+      </thead>
+      <tbody>
+      ${sortedAthletes.map(element => `
           <tr>
-            <th>Nome</th>
-            <th>Gênero</th>
-            <th>Altura</th>
-            <th>Peso</th>
-            <th>Esporte</th>
-            <th>País</th>
-            <th>Sigla</th>
-            <th>Idade</th>
-            <th>Evento</th>
-            <th>Medalha</th>
+            <td>${element.name}</td>
+            <td>${element.gender}</td>
+            <td>${element.height}</td>
+            <td>${element.weight}</td>
+            <td>${element.sport}</td>
+            <td>${element.team}</td>
+            <td>${element.noc}</td>
+            <td>${element.age}</td>
+            <td>${element.event}</td>
+            <td>${element.medal}</td>
           </tr>
-        </thead>
-        
-        <tbody>
-          ${sortedAthletes.map(element => `
-            <tr>
-              <td>${element.name}</td>
-              <td>${element.gender}</td>
-              <td>${element.height}</td>
-              <td>${element.weight}</td>
-              <td>${element.sport}</td>
-              <td>${element.team}</td>
-              <td>${element.noc}</td>
-              <td>${element.age}</td>
-              <td>${element.event}</td>
-              <td>${element.medal}</td>
-            </tr>
-          `).join('')}
-        </tbody>
-      </table>
-    `;
+        `).join('')}
+      </tbody>
+    </table>
+  `;
   }
 }
