fe9677b851e028563a1a90850b2c3bf0bf2e3c83
DanilaRamos <danila.ramos1985@gmail.com>
2023-03-12 15:16:46 -0300

    finaliza responsivo

--- src/participantes.js
+++ src/participantes.js
@@ -2,7 +2,6 @@ import {filterGender,filterData,filterAge,ascendingOrder,descendingOrder} from '
 import data from './data/athletes/athletes.js';
 
 const tableCreated = document.getElementById('tableFilter');
-const tableCreated = document.getElementById('tableFilter');
 const genderSelector = document.querySelector('#selectGender');
 const ageSelector = document.querySelector('#selectAge');
 const orderSelector = document.querySelector('#selectOrder'); 
@@ -17,6 +16,7 @@ endButton.onclick = () => {
     selectAllOptions.innerHTML = "⚠️ Por favor, selecione todas as opções antes de clicar em OK";
 
   } else {
+
     const gender = filterGender(arrayAthletes, "gender", genderSelector.value);
     const filterOne = filterAge(gender, ageSelector.value);
     const filtered = filterData(filterOne, "gender", genderSelector.value);
@@ -28,34 +28,19 @@ endButton.onclick = () => {
     }
   
     tableCreated.innerHTML =  `
-    <table>
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Gênero</th>
-          <th>Altura</th>
-          <th>Peso</th>
-          <th>Esporte</th>
-          <th>País</th>
-          <th>Sigla</th>
-          <th>Idade</th>
-          <th>Evento</th>
-          <th>Medalha</th>
-        </tr>
-      </thead>
-      <tbody>
-      ${sortedAthletes.map(element => `
+      <table>
+        <thead>
           <tr>
-            <td>${element.name}</td>
-            <td>${element.gender}</td>
-            <td>${element.height}</td>
-            <td>${element.weight}</td>
-            <td>${element.sport}</td>
-            <td>${element.team}</td>
-            <td>${element.noc}</td>
-            <td>${element.age}</td>
-            <td>${element.event}</td>
-            <td>${element.medal}</td>
+            <th>Nome</th>
+            <th>Gênero</th>
+            <th>Altura</th>
+            <th>Peso</th>
+            <th>Esporte</th>
+            <th>País</th>
+            <th>Sigla</th>
+            <th>Idade</th>
+            <th>Evento</th>
+            <th>Medalha</th>
           </tr>
         </thead>
         <tbody>
@@ -78,5 +63,4 @@ endButton.onclick = () => {
       </table>
     `;
   }
-
 }
